3c326c678a4f559be77ea51cddab841023b9066c
John Mason <john@johnmason.io>
2021-04-20 12:45:21 -0400

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="aww",
-    version="0.0.1",
+    version="0.0.2",
     author="John Mason",
     author_email="binarymason@users.noreply.github.com",
     description="AWS helper functions",
